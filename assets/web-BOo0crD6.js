7dac45c9d16055e5b7d7dd9750dfbd709a7dce0e
Sebastian Silva <sebastian@fuentelibre.org>
2024-12-19 15:17:11 -0500

    Updates

--- assets/web-BOo0crD6.js
+++
@@ -1,2 +0,0 @@
-import{W as e}from"./index-Sf6Pzqrg.js";class o extends e{constructor(){super(),this.registerWindowListener("devicemotion","accel"),this.registerWindowListener("deviceorientation","orientation")}}export{o as MotionWeb};
-//# sourceMappingURL=web-BOo0crD6.js.map
