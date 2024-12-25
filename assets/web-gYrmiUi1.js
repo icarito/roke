cd3d3359bb18a8b0a3e3988931adb996df8c9b13
Sebastian Silva <sebastian@fuentelibre.org>
2024-12-25 15:19:48 -0500

    Updates

--- assets/web-gYrmiUi1.js
+++
@@ -1,2 +0,0 @@
-import{W as e}from"./index-BMtAMTLT.js";class o extends e{constructor(){super(),this.registerWindowListener("devicemotion","accel"),this.registerWindowListener("deviceorientation","orientation")}}export{o as MotionWeb};
-//# sourceMappingURL=web-gYrmiUi1.js.map
