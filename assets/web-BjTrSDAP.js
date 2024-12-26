db69821b1e2983c76bb0c1cc3cd87f28ef99c962
Sebastian Silva <sebastian@fuentelibre.org>
2024-12-25 20:04:45 -0500

    Updates

--- assets/web-BjTrSDAP.js
+++
@@ -1,2 +0,0 @@
-import{W as e}from"./index-lsnqzjBA.js";class o extends e{constructor(){super(),this.registerWindowListener("devicemotion","accel"),this.registerWindowListener("deviceorientation","orientation")}}export{o as MotionWeb};
-//# sourceMappingURL=web-BjTrSDAP.js.map
