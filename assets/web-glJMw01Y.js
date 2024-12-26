1e6aebc65f6eb90b6687062ac89feff9844ff6fd
Sebastian Silva <sebastian@fuentelibre.org>
2024-12-25 20:47:43 -0500

    Updates

---
+++ assets/web-glJMw01Y.js
@@ -0,0 +1,2 @@
+import{W as e}from"./index-DcBX4sSl.js";class o extends e{constructor(){super(),this.registerWindowListener("devicemotion","accel"),this.registerWindowListener("deviceorientation","orientation")}}export{o as MotionWeb};
+//# sourceMappingURL=web-glJMw01Y.js.map
