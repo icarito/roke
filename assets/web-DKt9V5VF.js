7e89c031ae26b537d342cc5b993e767a801ab195
Sebastian Silva <sebastian@fuentelibre.org>
2024-12-26 09:37:54 -0500

    Updates

---
+++ assets/web-DKt9V5VF.js
@@ -0,0 +1,2 @@
+import{W as e}from"./index-1GCZeLpn.js";class o extends e{constructor(){super(),this.registerWindowListener("devicemotion","accel"),this.registerWindowListener("deviceorientation","orientation")}}export{o as MotionWeb};
+//# sourceMappingURL=web-DKt9V5VF.js.map
