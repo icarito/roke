a038abeb388d93017b983d0de8ee04f04a5c91ff
Sebastian Silva <sebastian@fuentelibre.org>
2024-12-28 01:03:38 -0500

    Updates

---
+++ assets/web-BAIm_Zw4.js
@@ -0,0 +1,2 @@
+import{W as e}from"./index-qE4YmnKo.js";class o extends e{constructor(){super(),this.registerWindowListener("devicemotion","accel"),this.registerWindowListener("deviceorientation","orientation")}}export{o as MotionWeb};
+//# sourceMappingURL=web-BAIm_Zw4.js.map
--- assets/web-WvzEyBzR.js
+++
@@ -1,2 +0,0 @@
-import{W as e}from"./index-I1w2YiGU.js";class o extends e{constructor(){super(),this.registerWindowListener("devicemotion","accel"),this.registerWindowListener("deviceorientation","orientation")}}export{o as MotionWeb};
-//# sourceMappingURL=web-WvzEyBzR.js.map
