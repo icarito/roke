8c141fef3dd78a9734c5ba65c468b3accbaa0ac9
Sebastian Silva <sebastian@fuentelibre.org>
2025-01-03 11:39:59 -0500

    Updates

--- assets/web-L0J_te_o.js
+++
@@ -1,2 +0,0 @@
-import{W as e}from"./index-CKrTgvR8.js";class o extends e{constructor(){super(),this.registerWindowListener("devicemotion","accel"),this.registerWindowListener("deviceorientation","orientation")}}export{o as MotionWeb};
-//# sourceMappingURL=web-L0J_te_o.js.map
