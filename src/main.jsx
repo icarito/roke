9c754c84e2c1a12b215c0dfdbd4b03f70724a6d9
Sebastian Silva <sebastian@fuentelibre.org>
2025-01-07 12:25:58 -0500

    Logo y más

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import * as ex from "excalibur";
 import { Player } from "./player";
 // import { Capsule } from "./capsule";
 // import { Coin } from "./money";
-import "./index.css";
 import { resources, dsml1 } from "./resources";
 import { addPermission } from "./permission";
+import { Terminal } from "@xterm/xterm";
+import { FitAddon } from "@xterm/addon-fit";
+import "@xterm/xterm/css/xterm.css";
+import "./index.css";
+import logo from "./assets/logo640.png";
 
 //ex.Flags.useCanvasGraphicsContext();
 
@@ -19,12 +23,39 @@ const game = new ex.Engine({
   },
 });
 const loader = new ex.Loader([dsml1, ...resources]);
+loader.playButtonText = 'NOCLIP ⚠️';
+loader.backgroundColor = '#020610';
+loader.logo = logo;
+loader.logoHeight = 480;
+loader.logoWidth = 640;
+loader.logoPosition = ex.vec(game.screen.width / 2 - 320, 0);
+loader.loadingBarPosition = ex.vec(game.screen.width / 2 - 320, 380);
+loader.loadingBarHeight = 10;
+loader.loadingBarColor = ex.Color.Yellow;
+loader.suppressPlayButton = true;
+
+let term = new Terminal({ fontSize: 20, fontFamily: "monospace", backgroundColor: "#020610" });
+let fit = new FitAddon();
+term.loadAddon(fit);
+term.open(document.getElementById('game_ui'))
+term.onKey(e => {
+  console.log(e.key);
+  term.write(e.key);
+  if (e.key == '\r')
+      term.write('\n');
+})
+
+fit.fit()
+
+term.write('Hello \x1B[1;3;31mR.O.K.E.\x1B[0m... ')
 
 document.body.addEventListener("click", function handler() {
   this.removeEventListener("click", handler);
   addPermission();
 });
-
+document.getElementById('game').onclick = (ev)=> {
+  ev.target.focus()
+}
 
 game.start(loader).then(() => {
   game.currentScene.physics.config.gravity = ex.vec(0, 200);
@@ -49,3 +80,4 @@ game.start(loader).then(() => {
 window.dsml1 = dsml1
 window.game = game
 window.ex = ex
+window.term = term
