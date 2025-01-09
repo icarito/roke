98598aa02ebf1488c47a8ffca2795ee74c6845a7
Sebastian Silva <sebastian@fuentelibre.org>
2025-01-08 20:58:47 -0500

    Updates

---
+++ assets/web-CtVG658u.js
@@ -0,0 +1,2 @@
+import{W as e}from"./index-kdFB3lPP.js";class o extends e{constructor(){super(),this.registerWindowListener("devicemotion","accel"),this.registerWindowListener("deviceorientation","orientation")}}export{o as MotionWeb};
+//# sourceMappingURL=web-CtVG658u.js.map
