bd1fa0f53543c61b3f9a7e6448f6c810f5d7f002
Sebastian Silva <sebastian@fuentelibre.org>
2025-01-08 21:26:32 -0500

    Updates

---
+++ assets/web-DbcYAaUC.js
@@ -0,0 +1,2 @@
+import{W as e}from"./index-BrT8LVZU.js";class o extends e{constructor(){super(),this.registerWindowListener("devicemotion","accel"),this.registerWindowListener("deviceorientation","orientation")}}export{o as MotionWeb};
+//# sourceMappingURL=web-DbcYAaUC.js.map
