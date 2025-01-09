b09ff4250259efc382778ec0ccb9aacc0837f687
Sebastian Silva <sebastian@fuentelibre.org>
2025-01-08 22:41:06 -0500

    Updates

---
+++ assets/web-BQL2583Z.js
@@ -0,0 +1,2 @@
+import{W as e}from"./index-NFcv5XO2.js";class o extends e{constructor(){super(),this.registerWindowListener("devicemotion","accel"),this.registerWindowListener("deviceorientation","orientation")}}export{o as MotionWeb};
+//# sourceMappingURL=web-BQL2583Z.js.map
