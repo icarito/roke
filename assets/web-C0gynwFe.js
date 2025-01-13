8b25adc58cb12f477126a1cfdd246ad781a0237f
Sebastian Silva <sebastian@fuentelibre.org>
2025-01-12 20:12:24 -0500

    Updates

---
+++ assets/web-C0gynwFe.js
@@ -0,0 +1,2 @@
+import{W as e}from"./index-CjC7kQ9w.js";class o extends e{constructor(){super(),this.registerWindowListener("devicemotion","accel"),this.registerWindowListener("deviceorientation","orientation")}}export{o as MotionWeb};
+//# sourceMappingURL=web-C0gynwFe.js.map
