7cf8ae59befcefaa2f6ec7710b238f61f6dc244f
Sebastian Silva <sebastian@fuentelibre.org>
2025-02-11 22:07:32 -0500

    🛠️ src/crtvisual.jsx -> Added state for position/size

--- src/crtvisual.jsx
+++ src/crtvisual.jsx
@@ -2,15 +2,46 @@ import { useEffect, useRef, useState } from "preact/hooks";
 import "winbox/dist/css/winbox.min.css";
 import "winbox/dist/css/themes/modern.min.css"; // optional
 import WinBox from "react-winbox";
-import { ReactTerminal } from "react-terminal"
+import { ReactTerminal } from "react-terminal";
 
 export function CrtVisual({ crtBox }) {
-  const [osd, setOsd] = useState("")
-  const termWin = useRef(null)
+  const [osd, setOsd] = useState("");
+  const termWin = useRef(null);
+  window._termWin = termWin;
+  const canvas = useRef(null);
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [width, setWidth] = useState(800);
+  const [height, setHeight] = useState(480);
 
-  useEffect(()=>{
-    termWin.current.minimize()
-  }, [])
+  useEffect(() => {
+    if (crtBox == null) return;
+    const winPos = termWin.current.getPosition();
+    const winSize = termWin.current.getSize();
+    if (winPos.y < crtBox.top) {
+      setY(crtBox.top);
+    }
+    if (winPos.y + winSize.height > crtBox.bottom) {
+      setHeight(crtBox.bottom - winPos.y);
+    } 
+    
+    if (winPos.x < crtBox.left) {
+      setX(crtBox.left);
+    } 
+    if (winPos.x + winSize.width > crtBox.right) {
+      setWidth(crtBox.right - winPos.x);  
+    }
+
+    if (termWin.current.isMax()) {
+      termWin.current.restore();
+    }
+    if (x + width > crtBox.right) {
+      setWidth(crtBox.right - x);
+    }
+    if (y + height > crtBox.bottom) {
+      setHeight(crtBox.bottom - y);
+    }
+  }, [crtBox]);
 
   function flash(output) {
     setOsd(output);
@@ -30,20 +61,14 @@ export function CrtVisual({ crtBox }) {
           background="#f005"
           ref={termWin}
           hide={crtBox == null}
-          /*width={crtBox?.right - crtBox?.left}
-          height={crtBox?.bottom - crtBox?.top}
-          x={crtBox?.left || "center"}
-          y={crtBox?.top || "center"}*/
+          width={width}
+          height={height}
+          x={x}
+          y={y}
           top={crtBox?.top || 0}
           left={crtBox?.left || 0}
           right={window.innerWidth - crtBox?.right || 0}
           bottom={window.innerHeight - crtBox?.bottom || 0}
-          onClose={()=>{
-            termWin.current.minimize()
-            return true
-          }}
-          noMax
-          noMin
           noFull
         >
           <ReactTerminal
@@ -54,7 +79,7 @@ export function CrtVisual({ crtBox }) {
         </WinBox>
       </div>
       <div id="game_margin">
-        <canvas tabIndex="1" id="game_canvas"></canvas>
+        <canvas ref={canvas} tabIndex="1" id="game_canvas"></canvas>
       </div>
     </div>
   );
